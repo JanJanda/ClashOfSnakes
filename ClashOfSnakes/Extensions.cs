f740066b125e43bbb08541df7cf83d0bc0e4a22c
Jan Janda <janjanda42@gmail.com>
2020-08-02 19:16:48 +0200

    Add a comment

--- ClashOfSnakes/Extensions.cs
+++ ClashOfSnakes/Extensions.cs
@@ -4,11 +4,17 @@ namespace ClashOfSnakes
 {
     static class Extensions
     {
-        public static int posMod(this int a, int divisor)
+        /// <summary>
+        /// Calculates positive remainder of a divided by divisor
+        /// </summary>
+        /// <param name="dividend"></param>
+        /// <param name="divisor"></param>
+        /// <returns>The ramainder, is not negative</returns>
+        public static int posMod(this int dividend, int divisor)
         {
             if (divisor <= 0) throw new ArgumentOutOfRangeException();
 
-            int tmp = a % divisor;
+            int tmp = dividend % divisor;
             return tmp < 0 ? tmp + divisor : tmp;
         }
     }
